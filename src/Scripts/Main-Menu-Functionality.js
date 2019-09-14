49dd6efabb78e2ea3c68459907959ec862706309
dbs-montoya <danmontoya001@gmail.com>
2019-09-14 00:10:42 -0400

    Updated README

--- src/Scripts/Main-Menu-Functionality.js
+++ src/Scripts/Main-Menu-Functionality.js
@@ -2,16 +2,41 @@
 //Main Menu Functionality
 //
 
-
+import { TimelineLite, TweenLite, Power0 } from 'gsap/all';
 
 
 (function(){
 
-    //Variables
+    //Variables -- -- -- -- -- -- -- --
         //Desktop
-            const MenuIcon = document.getElementById('Menu-Bar-Icon-Inner')
+            const MenuIcon = document.getElementById('Menu-Bar-Icon-Inner');
+            const MenuIconLine1 = document.getElementById('Menu-Bar-Icon-Line1');
+            const MenuIconLine2 = document.getElementById('Menu-Bar-Icon-Line2');
+            const MenuIconMenu = document.getElementById('Menu-Bar-Icon-Text-Menu');
+            const MenuIconClose = document.getElementById('Menu-Bar-Icon-Text-Close');
 
-    
+    //Variables -- -- -- -- -- -- -- --
+
+
+    //State -- -- -- -- -- -- -- --
+        //Main Menu
+            let MainMenuOpen = false;
+
+        //Main Menu Icon
+            let MainMenuIconActive = false;
+
+        //Main Menu
+
+
+
+
+
+    //Tweens -- -- -- -- -- -- -- --
+
+        //Menu Icon Hover
+            TweenLite.to(MenuIcon, .3, {opacity: 0, 'z-index': 1, ease: Power0.ease});
+            TweenLite.to(MenuIconMenu, .3, {opacity: 0, 'z-index': 1, ease: Power0.ease});
+
 
     
     console.log('Test From Main Menu Func');
