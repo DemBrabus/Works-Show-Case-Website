021c25593ae357ac1bce4ac879c2c4bb158bb2c8
dbs-montoya <danmontoya001@gmail.com>
2019-09-15 21:59:56 -0400

    started landing intro, Need to determine UI layout before carrying out this build..

--- src/Page-Index/index.js
+++ src/Page-Index/index.js
@@ -10,6 +10,9 @@ import MainMenuFunc from '../Scripts/MainMenu/Functionality';
 import '../Styles/xxStyles.scss';
 import '../Styles/Navigation/MenuBar(s).scss';
 import '../Styles/Navigation/MainMenu.scss';
+import '../Styles/ContentWindow/DesktopContentWindow.scss';
+import '../Styles/LandingLayers/LandingIconLayer.scss';
+import '../Styles/LandingLayers/LandingPreview.scss';
 import './index.scss';
 
 console.log('Hello from Index.js');
